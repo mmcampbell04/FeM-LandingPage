1f7dd44b0dbfebf2909b2138813c20154adc3028
Meghan McCall-Campbell <meghan.mccall.campbell@gmail.com>
2022-05-02 22:24:07 +0100

    added sticky nav with background on scroll

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const toggleBtn = document.getElementById("btn-toggle");
 const mobileMenu = document.getElementById("navbar-menu");
 const body = document.getElementById("home");
+const nav = document.getElementById("nav");
 
 toggleBtn.addEventListener("click", function () {
   mobileMenu.classList.toggle("active");
   body.classList.toggle("active");
   toggleBtn.classList.toggle("animate");
 });
+
+function setStickykNav() {
+  const stickyNav = nav.offsetTop;
+  const scrollHeight = window.pageYOffset;
+  if (scrollHeight > stickyNav) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+
+  console.log(stickyNav);
+  console.log(scrollHeight);
+}
+
+window.addEventListener("scroll", setStickykNav);
